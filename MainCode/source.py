be5e41b396efa39832e9314a2e0c0674ce17a8f4
jmhymas <56490065+jmhymas@users.noreply.github.com>
2019-10-12 15:59:22 -0700

    Updated python class documents

---
+++ MainCode/source.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 12 11:56:46 2019
+
+@author: Sector House 2814
+"""
+
+
+import circleClass #imports class file
+import rectangleClass
+
+x = circleClass.myCircle(5) #declares x as an instance of class myCircle     
+y = rectangleClass.myRectangle(1,2) #declares y as an instance of class myRectangle
+
+
